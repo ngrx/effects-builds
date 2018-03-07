9139f3ea75bd002b171cd4be6ff6a1302fa751af
Brandon <robertsbt@gmail.com>
2018-03-07 00:51:23 +0000

    d98915f chore(build): Generate named umd bundles (#893)

--- bundles/effects-testing.umd.js
+++ bundles/effects-testing.umd.js
@@ -1,6 +1,6 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@ngrx/effects'), require('rxjs/observable/defer')) :
-	typeof define === 'function' && define.amd ? define(['exports', '@ngrx/effects', 'rxjs/observable/defer'], factory) :
+	typeof define === 'function' && define.amd ? define('@ngrx/effects/testing', ['exports', '@ngrx/effects', 'rxjs/observable/defer'], factory) :
 	(factory((global.ngrx = global.ngrx || {}, global.ngrx.effects = global.ngrx.effects || {}, global.ngrx.effects.testing = {}),global.ngrx.effects,global.Rx.Observable.defer));
 }(this, (function (exports,effects,defer) { 'use strict';
 
