b21c49423ba02425f15e5324ad81203e9347b36e
GitHub <noreply@github.com>
2023-10-19 12:46:50 +0000

    3dbcadc8 feat(effects): deprecate act operator (#4073)

--- src/act.d.ts
+++ src/act.d.ts
@@ -8,5 +8,13 @@ export interface ActConfig<Input, OutputAction extends Action, ErrorAction exten
     operator?: <Input, OutputAction>(project: (input: Input, index: number) => Observable<OutputAction>) => OperatorFunction<Input, OutputAction>;
     unsubscribe?: (count: number, input: Input) => UnsubscribeAction;
 }
+/**
+ * @deprecated Use plain RxJS operators instead.
+ * For more info see: https://github.com/ngrx/platform/issues/4072
+ */
 export declare function act<Input, OutputAction extends Action, ErrorAction extends Action>(project: (input: Input, index: number) => Observable<OutputAction>, error: (error: any, input: Input) => ErrorAction): (source: Observable<Input>) => Observable<OutputAction | ErrorAction>;
+/**
+ * @deprecated Use plain RxJS operators instead.
+ * For more info see: https://github.com/ngrx/platform/issues/4072
+ */
 export declare function act<Input, OutputAction extends Action, ErrorAction extends Action, CompleteAction extends Action = never, UnsubscribeAction extends Action = never>(config: ActConfig<Input, OutputAction, ErrorAction, CompleteAction, UnsubscribeAction>): (source: Observable<Input>) => Observable<OutputAction | ErrorAction | CompleteAction | UnsubscribeAction>;
